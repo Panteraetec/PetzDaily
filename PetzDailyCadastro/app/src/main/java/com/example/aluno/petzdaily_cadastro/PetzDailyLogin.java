0160c3f4651a5ec14c7c03aa8f1ae12da228be95
Panteraetec <panterahas@gmail.com>
2019-03-28 22:32:46 -0300

    Modificação 28/03 - Lucas

--- PetzDailyCadastro/app/src/main/java/com/example/aluno/petzdaily_cadastro/PetzDailyLogin.java
+++ PetzDailyCadastro/app/src/main/java/com/example/aluno/petzdaily_cadastro/PetzDailyLogin.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PetzDailyLogin extends AppCompatActivity {
+
+    Button btnAcessar;
 
 
     @Override
@@ -14,10 +17,21 @@ public class PetzDailyLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petz_daily_login);
 
+        btnAcessar = (Button) findViewById(R.id.btnAcessar);
+
     }
+
+
+
 
     public void onClickRegistrar(View v){
         Intent it = new Intent(PetzDailyLogin.this , MainActivity.class);
+
+        startActivity(it);
+    }
+
+    public void onClickAcessar(View v){
+        Intent it = new Intent(PetzDailyLogin.this , ActivityPetzDailyNavigation.class);
 
         startActivity(it);
     }
