db009233c884bf33dc0d42fc4cc6749196b8267c
panteraetec <panterahas@gmail.com>
2019-03-29 18:48:06 -0300

    MODIFICAÇÃO 29/03/2019 - EDUARDO

---
+++ PetzDailyCadastro/app/src/main/java/com/example/aluno/petzdaily_cadastro/petz_eventos.java
@@ -0,0 +1,13 @@
+package com.example.aluno.petzdaily_cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class petz_eventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_petz_eventos);
+    }
+}
